c241e129fd1604da3869ac260827c2ed1fa7c9d3
MAlfanR <alfanriyadi25@gmail.com>
2020-08-07 23:32:19 +0700

    CoderClass 1

---
+++ Coder Class Compfest/hariPenimbangan.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+double a,b,c,d,e,f;
+
+cin>>a>>b>>c>>d>>e>>f;
+
+double hasil;
+
+hasil = (a+b+c+d+e+f)/6;
+
+cout<<fixed<<setprecision(8)<<hasil<<endl;
+}
